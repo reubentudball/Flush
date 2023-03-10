15edb598571a8dee109e487e3eee25809552d23d
Reuben Tudball <reuben_tudball@hotmail.com>
2023-03-10 18:48:17 -0500

    sorting options added

--- lib/SearchPage.dart
+++ lib/SearchPage.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+const List<String> sortBy = <String>['Nearest', 'Most Clean', 'Most Quiet', 'Most Accessible'];
+
 
 class SearchPage extends StatefulWidget{
   @override
@@ -10,24 +14,68 @@ class _SearchPageState extends State<SearchPage>{
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Search Page"),
+        title: Text("Find Bathroom"),
       ),
       body: SingleChildScrollView(
-        child: TextField(
-          decoration: InputDecoration(
-            hintText: 'Search for location',
-            hintStyle: TextStyle(
-              color: Colors.black,
-              fontSize: 18,
-              fontStyle: FontStyle.italic,
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              child: Padding(
+              padding: const EdgeInsets.only(left: 20),
+
+
+            child :TextField(
+              decoration: InputDecoration(
+                hintText: 'Search for location',
+                hintStyle: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 18,
+                  fontStyle: FontStyle.italic,
+                ),
+              ),
+              style: TextStyle(
+                color: Colors.black,
+              ),
             ),
           ),
-          style: TextStyle(
-            color: Colors.black,
-          ),
-        ),
+            ),
+            SortingDropDown(),
+
+          ],
+        )
+
       )
     );
   }
+}
 
+class SortingDropDown extends StatefulWidget{
+  const SortingDropDown({super.key});
+
+  @override
+  _SortingDropDownState createState() => _SortingDropDownState();
+}
+
+class _SortingDropDownState extends State<SortingDropDown>{
+  String defaultValue = sortBy.first;
+
+  @override
+  Widget build(BuildContext context){
+    return DropdownButton<String>(
+      value: defaultValue,
+      icon: const Icon(Icons.sort),
+      
+      onChanged: (String? value) {
+        setState(() {
+          defaultValue = value!;
+        });
+      },
+      items: sortBy.map<DropdownMenuItem<String>>((String value){
+        return DropdownMenuItem<String>(
+          value: value,
+          child: Text(value)
+        );
+      }).toList(),
+    );
+  }
 }
