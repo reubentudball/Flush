495fc59416e7b92c2ee1e68758497f7facb3f28c
Tyler Makris <makrist@sheridancollege.ca>
2023-11-13 19:49:25 -0500

    Added icon button to toggle torch

--- lib/QrCodeScanner.dart
+++ lib/QrCodeScanner.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
 
 
-class QrCodeScanner extends StatelessWidget {
+class QrCodeScanner extends StatefulWidget {
+  @override
+  _QrCodeScannerState createState() => _QrCodeScannerState();
+}
+
+class _QrCodeScannerState extends State<QrCodeScanner> {
   MobileScannerController cameraController = MobileScannerController();
   bool _screenOpened = false;
 
@@ -11,6 +16,24 @@ class QrCodeScanner extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Qr Code Scanner'),
+        actions: [
+          IconButton(
+            color: Colors.white,
+            icon: ValueListenableBuilder(
+              valueListenable: cameraController.torchState,
+              builder: (context, state, child) {
+                switch (state as TorchState) {
+                  case TorchState.off:
+                    return const Icon(Icons.flashlight_off, color: Colors.grey);
+                  case TorchState.on:
+                    return const Icon(Icons.flashlight_on, color: Colors.yellow);
+                }
+              },
+            ),
+            iconSize: 32.0,
+            onPressed: () => cameraController.toggleTorch(),
+          ),
+        ],
       ),
       body: MobileScanner(onDetect: (BarcodeCapture barcodes) {  },
 
@@ -18,4 +41,5 @@ class QrCodeScanner extends StatelessWidget {
       ),
     );
   }
+
 }
