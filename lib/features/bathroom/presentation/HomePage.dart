5b90c4514bd6c20b44d828f2350847a0f9701413
Reuben Tudball <reuben_tudball@hotmail.com>
2024-11-29 15:54:59 -0500

    updated filter options

--- lib/features/bathroom/presentation/HomePage.dart
+++ lib/features/bathroom/presentation/HomePage.dart
@@ -214,10 +214,6 @@ class _HomePageState extends State<HomePage> {
               },
               itemBuilder: (context) => [
                 const PopupMenuItem(
-                  value: 0.1,
-                  child: Text("Search within 100 m")
-                ),
-                const PopupMenuItem(
                   value: 0.5,
                   child: Text("Search within 0.5 km"),
                 ),
